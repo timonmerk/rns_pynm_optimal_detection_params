f4f96ee012bc92672222ac4ac23054b786066746
timonmerk <timon.merk@charite.de>
2023-01-26 08:59:32 +0100

    remove next PE predictions

---
+++ understand/read_performances.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import os
+
+df = pd.read_pickle(os.path.dirname(__file__) + '\\..\\performances_RNS_bandpass.p')
+
+print(df)
+
+
+df_settings = pd.read_csv(os.path.dirname(__file__) + "\\..\\iESPNet_bandpass_parameters_PythonMerge.csv")
